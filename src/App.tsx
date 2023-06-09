2b85125956723a80ff09eb343649f94e2fdde8b6
Junar Alinsub <junaralinsub2@protonmail.com>
2023-06-09 08:52:37 +0800

    :construction: WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import Game from "./components/pages/Game";
 import Landing from "./components/pages/Landing";
 import GameProvider from "./components/provider/GameProvider";
 
+export const ROOT_URL = "/tic-tac-shuffle";
+
 const App = () => {
   const pathname = window.location.pathname;
 
+  if (pathname === "/") {
+    return (window.location.href = ROOT_URL);
+  }
+
   return (
     <div className="App">
-      {pathname === "/" ? (
+      {pathname === ROOT_URL ? (
         <Landing />
       ) : (
         <GameProvider>
