45e2d08565e178d9bc15a2d8bb15ee73fd924a9b
Junar Alinsub <junaralinsub2@protonmail.com>
2023-06-22 14:40:02 +0800

    :ambulance: Fix game, Fix minimax

--- src/components/helpers/minimax.ts
+++ src/components/helpers/minimax.ts
@@ -1,18 +1,12 @@
 import { winningCombinations } from "../../schema";
 import { Player } from "../types/types";
 
-export const emptySquares = (currentBoard: Record<string, string>) => {
-  if (!!currentBoard) {
-    const emptyCells = [];
-    for (let i = 0; i < 10; i += 1) {
-      if (!currentBoard[i]) {
-        emptyCells.push(i);
-      }
-    }
-    return emptyCells;
-  }
-  return [0, 1, 2, 3, 4, 5, 6, 7, 8];
-};
+export const emptySquares = (currentBoard: Record<string, string>) =>
+  !!currentBoard
+    ? Array.from(Array(9))
+        .map((_, i) => i)
+        .filter((i) => !currentBoard[i])
+    : Array.from(Array(9)).map((_, i) => i);
 
 export const checkWin = (
   player: Player,
@@ -32,7 +26,7 @@ export const checkWin = (
 
 export const minimax = ({ currentBoard, player, playerClass }: any) => {
   let availSpots = emptySquares(currentBoard);
-  let newCArr = { ...currentBoard };
+  let newBoard = { ...currentBoard };
 
   if (checkWin("p1", currentBoard, playerClass)) {
     return { score: -10 };
@@ -46,10 +40,26 @@ export const minimax = ({ currentBoard, player, playerClass }: any) => {
 
   for (let i = 0; i < availSpots.length; i += 1) {
     let move: any = {};
-    newCArr[availSpots[i]] = playerClass[player];
 
-    let result = minimax({ currentBoard: newCArr, player, playerClass });
-    move.score = result.score;
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = playerClass[player];
+
+    if (player === "p1") {
+      const { score } = minimax({
+        currentBoard: newBoard,
+        player: "p2",
+        playerClass,
+      });
+      move.score = score;
+    } else {
+      const { score } = minimax({
+        currentBoard: newBoard,
+        player: "p1",
+        playerClass,
+      });
+
+      move.score = score;
+    }
 
     moves.push(move);
   }
@@ -57,7 +67,7 @@ export const minimax = ({ currentBoard, player, playerClass }: any) => {
   let bestMove = 0;
   if (player === "p2") {
     let bestScore = -10000;
-    for (let i = 0; i < moves.length; i++) {
+    for (let i = 0; i < moves.length; i += 1) {
       if (moves[i].score > bestScore) {
         bestScore = moves[i].score;
         bestMove = i;
@@ -65,7 +75,7 @@ export const minimax = ({ currentBoard, player, playerClass }: any) => {
     }
   } else {
     let bestScore = 10000;
-    for (let i = 0; i < moves.length; i++) {
+    for (let i = 0; i < moves.length; i += 1) {
       if (moves[i].score < bestScore) {
         bestScore = moves[i].score;
         bestMove = i;
@@ -73,9 +83,8 @@ export const minimax = ({ currentBoard, player, playerClass }: any) => {
     }
   }
 
-  console.log(newCArr, availSpots, bestMove, availSpots[bestMove]);
   return {
-    index: newCArr[availSpots[bestMove]],
+    index: availSpots[bestMove],
     score: moves[bestMove].score,
   };
 };
--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  datebaseURL: process.env.REACT_APP_DATABASE_URL,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
